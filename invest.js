d1f0d9a2e72b605902b6e5593b30d4cb8f91792a
Derek Robbins <derekseanrobbins@gmail.com>
2015-10-22 21:38:46 -0700

    basic functionallity with console.log output

--- invest.js
+++ invest.js
@@ -1,37 +1,101 @@
 App = (function() {
     'use strict';
 
-    var App = {
-        vars: {},
-        init: init
-    };
+    var bank, down, closing, monthlyCostMortgaged, monthlyCostOwned, monthlyPayment, rent, tax, costToBuy,
+        vars = {},
+        propertiesWithMortgages = [],
+        propertiesOwned = [];
+
     function init() {
-        initVars();
+        console.log('init');
+        bank = vars.initial;
+
+        // wild ass guess
+        closing = 0.016 * vars.price;
+        down = closing + vars.price * vars.downPayment / 100;
+        rent = vars.price * vars.rentToPrice;
+        monthlyPayment = new Property().getMonthlyPayment();
+        tax = vars.price * (vars.tax / 100) / 12;
+        monthlyCostMortgaged = (monthlyPayment + vars.hoi / 12 + tax + vars.pm) * (1 + vars.vacancy / 100);
+        monthlyCostOwned = (vars.hoi / 12 + tax + vars.pm) * (1 + vars.vacancy / 100);
+        costToBuy = down + closing + monthlyPayment * 6;
+        propertiesWithMortgages = [];
+        propertiesOwned = [];
         calculate();
+        console.log(rent);
     }
     function calculate() {
-        var months = App.vars.years * 12;
-        var i;
+        var months = vars.years * 12;
+        var income = 0;
+        var payment;
+        var totalInterestPaid = 0;
+        var taxableIncome;
+        var mortgage;
+        var totalProperties;
+        var i, j;
         for(i = 0; i < months; i++) {
+            income = 0;
+            taxableIncome = 0;
+            totalInterestPaid = 0;
 
+            // first pay off any liabilities
+            bank = bank - monthlyCostOwned * propertiesOwned.length;
+            for(j = 0; j < propertiesWithMortgages.length; j++) {
+                bank = bank - monthlyCostMortgaged;
+                mortgage = propertiesWithMortgages[j];
+                payment = mortgage.makePayment();
+                totalInterestPaid += payment.interest;
+                income += payment.refund;
+                if(payment.refund) {
+                    propertiesOwned.push(propertiesWithMortgages.splice(j, 1));
+                }
+            }
+            income += rent * (propertiesOwned.length + propertiesWithMortgages.length);
+            taxableIncome = income - totalInterestPaid;
+            bank += totalInterestPaid + taxableIncome * (1 - vars.incometax / 100);
+            bank += vars.external;
+            if(!(i % 12)) {
+                console.log('year ' + i / 12 + ': ' + (propertiesOwned.length + propertiesWithMortgages.length) + ' properties owned');
+                //console.log('income: ' + (totalInterestPaid + taxableIncome * (1 - vars.incometax / 100)));
+            }
+            // buy buy buy
+            while(bank >= costToBuy) {
+                buyProperty()
+            }
         }
+        console.log('bank: ', bank);
+        console.log('income: ' + (totalInterestPaid + taxableIncome * (1 - vars.incometax / 100)));
+    }
+    function buyProperty() {
+        propertiesWithMortgages.push(new Property());
+        bank = bank - costToBuy;
     }
     function initVars() {
         var elements = {
             initial: document.getElementById('initial'),
+            external: document.getElementById('external'),
             rentToPrice: document.getElementById('rent-to-price'),
             downPayment: document.getElementById('down-payment'),
+            incometax: document.getElementById('incometax'),
             price: document.getElementById('price'),
-            years: document.getElementById('years')
+            years: document.getElementById('years'),
+            tax: document.getElementById('tax'),
+            interest: document.getElementById('interest'),
+            term: document.getElementById('term'),
+            hoi: document.getElementById('hoi'),
+            pm: document.getElementById('pm'),
+            vacancy: document.getElementById('vacancy')
         };
         var el, key;
         for(key in elements) {
             if(elements.hasOwnProperty(key)) {
-                Object.defineProperty(App.vars, key, {
-                    get: function() {
-                        return elements[key].value;
-                    }
-                });
+                (function(key) {
+                    Object.defineProperty(vars, key, {
+                        get: function() {
+                            return Number(elements[key].value);
+                        }
+                    });
+                })(key);
             }
         }
     }
@@ -39,10 +103,10 @@ App = (function() {
     // Define propery Object
     var Property = function(options) {
         this.options = {
-            price: 100000,
-            down: 20000,
-            interestRate: 0.04,
-            term: 30
+            price: vars.price,
+            down: vars.price * vars.downPayment / 100,
+            interestRate: (vars.interest / 100) / 12,
+            term: vars.term
         };
         var key;
         for(key in options) {
@@ -51,22 +115,52 @@ App = (function() {
         this.init();
     };
     Property.prototype.init = function() {
+        this.payments = 0;
         this.equity = this.options.down;
+        this.principal = this.options.price - this.options.down;
+        this.c = this.getMonthlyPayment();
         this.totalCost = this.getTotalCost();
     };
-    Property.prototype.getEquityAtMonth = function(month) {
+    Property.prototype.makePayment = function() {
+        var interest = this.principal * this.options.interestRate;
+        var payDown = this.c - interest;
+        var refund = 0;
+        this.payments++;
+        if(this.principal <= payDown) {
+            refund = payDown - this.principal;
+            this.principal = 0;
+            console.log('payed off!!! in ' + (this.payments / 12) + ' years');
+        } else {
+            this.principal -= payDown;
+        }
+        return {
+            interest: interest,
+            refund: refund
+        };
+    }
+    Property.prototype.getEquityAtEndOfMonth = function(month) {
+        var r = this.options.interestRate;
+        var P = this.options.price - this.options.down;
+        var N = month;
+        var amountOwed = Math.pow((1 + r), month) * P;
+        return this.options.price - amountOwed;
     };
-    Property.prototype.getEquityAtYear = function(year) {
+    Property.prototype.getEquityAtEndOfYear = function(year) {
         return this.getEquityAtMonth(year * 12);
     };
     Property.prototype.getMonthlyPayment = function() {
-        console.log(this);
-        return this.totalCost / this.options.term / 12;
+        var r = this.options.interestRate;
+        var P = this.options.price - this.options.down;
+        var N = this.options.term * 12;
+        return r * P / (1 - Math.pow((1 + r), N * -1));
     };
     Property.prototype.getTotalCost = function() {
-        return (this.options.price - this.options.down) * Math.pow((1 + this.options.interestRate / 12), this.options.term * 12);
+        return this.c * this.options.term * 12;
     };
-    App.init();
-    App.Property = Property;
-    return App;
+    initVars();
+    init();
+    document.getElementById('recalculate').onclick = init;
+    return {
+        Property: Property
+    };
 })();
